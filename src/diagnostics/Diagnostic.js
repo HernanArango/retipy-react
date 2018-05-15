391a243a96cb7fbab6507c23c0ed8119d3c447c2
Alejandro Valdés <alejandrovaldes@live.com>
2018-05-14 22:30:22 -0500

    Added several missing features for Diagnostic

--- src/diagnostics/Diagnostic.js
+++ src/diagnostics/Diagnostic.js
@@ -9,18 +9,39 @@ import Paper from '@material-ui/core/Paper';
 import Switch from '@material-ui/core/Switch';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { Typography } from "@material-ui/core";
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    margin: theme.spacing.unit,
   },
   paper: {
     padding: theme.spacing.unit * 2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  roiButton: {
+    margin: theme.spacing.unit / 2,
+  },
   button: {
     margin: theme.spacing.unit,
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  close: {
+    width: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 4,
   },
 });
 
@@ -75,15 +96,19 @@ class Diagnostic extends Component
   state =
   {
     id: 0,
-    width: 0,
-    height: 0,
+    width: 600,
+    height: 600,
+    ratio: 1,
     notes: "",
     rois: [],
     roiCount: 0,
     diagnostic: "",
     points: [],
     roiText: "",
-    addingRoi: false
+    addingRoi: false,
+    creationDate: "",
+    showSnackbar: false,
+    userMessage: ""
   }
 
   constructor(props)
@@ -94,16 +119,77 @@ class Diagnostic extends Component
 
   componentDidMount()
   {
-
+    // Load the data from the rest endpoint
     getId(this.state.id).then(data =>
       {
+        //image load
         const image = document.createElement('img');
         image.onload = () =>
         {
-          this.setState({width: image.naturalWidth, height: image.naturalHeight});
-          this.setState({ image });
+          const initial_width = image.naturalWidth;
+          const initial_height = image.naturalHeight;
+          var ratio = 1;
+          var width = image.naturalWidth;
+          var height = image.naturalHeight;
+          if (width > this.state.width)
+          {
+            ratio = this.state.width / initial_width;
+            height = initial_height * ratio;
+            width = initial_width * ratio;
+            if (height > this.state.height)
+            {
+              ratio = this.state.height / initial_height;
+              width = initial_width * ratio;
+              height = initial_height * ratio;
+            }
+          }
+          else if(height > this.state.height)
+          {
+            ratio = this.state.height / initial_height;
+            width = initial_width * ratio;
+            height = initial_height * ratio;
+          }
+          console.log(`image is resized ${ratio}%`);
+
+
+          // extra data load
+          const dataRoiList = data.rois;
+          var roiList = [];
+          for (var i=0; i < dataRoiList.length; i++)
+          {
+            var currentRoi = dataRoiList[i];
+            var points = [];
+            for (var pIndex=0; pIndex < currentRoi.roi_x.length; pIndex++)
+            {
+              points.push(currentRoi.roi_x[pIndex] * ratio);
+              points.push(currentRoi.roi_y[pIndex] * ratio);
+            }
+            const roi = <PolyRoi
+              tooltip={i}
+              text={currentRoi.notes}
+              key={i}
+              points={points} />
+            roiList.push(roi);
+          }
+          this.setState(
+            {
+              height: height,
+              width: width,
+              ratio: ratio,
+              image: image,
+              rois: roiList
+            });
         }
         image.src = "data:image/png;base64," + data.image;
+
+        this.setState(
+          {
+            diagnostic: data.diagnostic,
+            creationDate: data.creationDate,
+            showSnackbar: true,
+            userMessage: "Diagnostic Loaded Successfully"
+          });
+
       });
   }
 
@@ -131,12 +217,20 @@ class Diagnostic extends Component
     this.setState( { points: [], addingRoi: false } );
   }
 
+  handleUndoLastPoint = () =>
+  {
+    const pLength = this.state.points.length
+    if (pLength >= 2)
+    {
+      this.setState({points: this.state.points.slice(0, pLength-2)})
+    }
+  }
+
   handleSaveRoi = () =>
   {
     if (this.state.addingRoi && this.state.points.length >= 6)
     {
       const id = this.state.roiCount;
-      console.log(`saving roi with id ${id}`);
       const roi =
         <PolyRoi tooltip={id} text={this.state.roiText} key={id} points={this.state.points} />
       var roiList = this.state.rois;
@@ -147,8 +241,42 @@ class Diagnostic extends Component
           roiCount: id + 1,
           points: [],
           addingRoi: false,
-          roiText: ""
+          roiText: "",
+          showSnackbar: true,
+          userMessage: "Added RoI"
         });
+    }
+  }
+
+  handleSnackbarClose = (event, reason) =>
+  {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState(
+      {
+        showSnackbar: false,
+        userMessage: ""
+      });
+  }
+
+  handleAddRoiNotes = event =>
+  {
+    this.setState({roiText: event.target.value});
+  }
+
+  handleUpdateDiagnostic = event =>
+  {
+    if (this.state.addingRoi)
+    {
+      this.setState(
+        {
+          showSnackbar: true,
+          userMessage: "Please Add or Cancel the current RoI"
+        }
+      )
+      return;
     }
   }
 
@@ -156,9 +284,37 @@ class Diagnostic extends Component
   {
     const { classes } = this.props;
     return(
-      <Grid container className={classes.root} spacing={24}>
-        <Grid item justify="center" xs={12}> {/* Konva div */}
-        <div>
+      <div className={classes.root}>
+      <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={this.state.showSnackbar}
+          autoHideDuration={3000}
+          onClose={this.handleSnackbarClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={<span id="message-id">{this.state.userMessage}</span>}
+          action={
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              className={classes.close}
+              onClick={this.handleSnackbarClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          }
+        />
+
+
+      <Grid container spacing={24} justify="center">
+        <Grid item xs={12}> {/* Konva div */}
+        <Paper className={classes.paper}>
+          <Typography variant="display1">Diagnostic</Typography>
           <Stage
             width={this.state.width}
             height={this.state.height}
@@ -184,53 +340,112 @@ class Diagnostic extends Component
               {this.state.rois}
             </Layer>
           </Stage>
-        </div>
+        </Paper>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="display1">New Region of Interest</Typography>
             <Grid container>
-              <Grid item>
-                <Paper className={classes.paper}>
-                  <FormGroup row>
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={this.state.addingRoi}
-                          color="primary"
-                          onChange={this.handleAddRoi}
-                        />}
-                      label="Add new ROI"
-                    />
-                    <Button
-                      variant="raised"
-                      color="primary"
-                      onClick={this.handleSaveRoi}
-                      disabled={!this.state.addingRoi}
-                      className={classes.button}
-                    >
-                      Save
-                    </Button>
-                    <Button
-                      variant="raised"
-                      color="primary"
-                      onClick={this.handleClearRoi}
-                      disabled={!this.state.addingRoi}
-                      className={classes.button}
-                    >
-                      Cancel
-                    </Button>
-                  </FormGroup>
-                  <TextField
+              <Grid item xs={2}>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={this.state.addingRoi}
                     color="primary"
-                    disabled={!this.state.addingRoi}
-                    placeholder="Notes"
-                    helperText="Roi Observation"
-                    value={this.state.roiText}
-                  />
-                </Paper>
+                    onChange={this.handleAddRoi}
+                  />}
+                label="Add new"
+              />
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  onClick={this.handleUndoLastPoint}
+                  disabled={!this.state.addingRoi}
+                  className={classes.roiButton}
+                >
+                  Undo Last Point
+                </Button>
+              </Grid>
+              <Grid item xs={1}>
+                <TextField
+                      value={this.state.points.length/2}
+                      label="Points"
+                      disabled
+                    />
               </Grid>
             </Grid>
+            <TextField
+              required
+              id="roiNotes"
+              disabled={!this.state.addingRoi}
+              placeholder="eg. Dead Tissue"
+              helperText="Roi Observation"
+              value={this.state.roiText}
+              onChange={this.handleAddRoiNotes}
+              margin="normal"
+              label="Notes"
+              fullWidth
+            />
+              <Button
+                variant="raised"
+                type="submit"
+                onClick={this.handleSaveRoi}
+                disabled={!this.state.addingRoi}
+                className={classes.roiButton}
+              >
+                Save
+              </Button>
+              <Button
+                color="primary"
+                onClick={this.handleClearRoi}
+                disabled={!this.state.addingRoi}
+                className={classes.roiButton}
+              >
+                Cancel
+              </Button>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="display1">Diagnostic Information</Typography>
+            <Grid container>
+              <Grid item xs={6}>
+              <TextField
+                value={this.state.rois.length}
+                label="RoI Count"
+                className={classes.textField}
+                disabled/>
+              </Grid>
+              <Grid item xs={6}>
+              <TextField
+                value={ new Date(this.state.creationDate).toDateString()}
+                label="Date Added"
+                className={classes.textField}
+                InputLabelProps={{ shrink: true, }}
+                disabled/>
+              </Grid>
+              <Grid item xs={12}>
+              <TextField
+                value={this.state.diagnostic}
+                label="Diagnostic"
+                helperText="Most prominent disease"
+                onChange={e => this.setState({diagnostic: e.target.value})}
+                fullWidth/>
+              </Grid>
+              <Grid item xs={12}>
+              <Button
+                variant="raised"
+                color="primary"
+                classes={classes.button}
+                onClick={this.handleUpdateDiagnostic}>
+                Update Diagnostic
+              </Button>
+              </Grid>
+            </Grid>
+          </Paper>
         </Grid>
       </Grid>
+      </div>
     );
   }
 }
